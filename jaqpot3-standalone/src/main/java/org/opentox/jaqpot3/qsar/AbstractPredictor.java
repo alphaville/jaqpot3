1dae7678783227a842d2657e74e26c2f25ab84c1
philip <philip@147.102.82.233>
2014-07-30 11:48:25 +0300

    comments

--- jaqpot3-standalone/src/main/java/org/opentox/jaqpot3/qsar/AbstractPredictor.java
+++ jaqpot3-standalone/src/main/java/org/opentox/jaqpot3/qsar/AbstractPredictor.java
@@ -166,7 +166,7 @@ public abstract class AbstractPredictor implements IPredictor {
     public SubstanceDataset predictEnm(VRI input) throws JaqpotException {
         Instances inst = predictInstances(input);
         SubstanceDataset ds = new SubstanceDataset();
-        //TODO IMPORTANT!!! this is custom 
+        //TODO IMPORTANT!!! this is custom must be generic
         String csvData = WekaInstancesProcess.getCSVOutput(inst,input,"http://apps.ideaconsult.net:8080/enanomapper/");
         ds.setCsv(csvData);
         return ds;
